17062c2a9f0e5ee46baa13c1b9e8372dc8c11d72
Caglar Cankaya <caglarcankaya@yandex.com>
2020-03-15 19:44:57 +0300

    Bugs fixed on religion class operations

--- src/Model/Religion.java
+++ src/Model/Religion.java
@@ -11,21 +11,24 @@ public class Religion {
     int[] powerAwardPositions; //placed /wrt the higher value of track number
 
     Religion(int playerCount, int[] initial_religion_points){
-        orderOfCult_3 = 0;
-        orderOfCult_2_1 = 0;
-        orderOfCult_2_2 = 0;
-        orderOfCult_2_3 = 0;
+        orderOfCult_3 = -1;
+        orderOfCult_2_1 = -1;
+        orderOfCult_2_2 = -1;
+        orderOfCult_2_3 = -1;
         playerPositions = new int[playerCount];
-        powerAwardPositions = new int[4];
-        powerAwardPositions[0] = 3;
-        powerAwardPositions[1] = 5;
-        powerAwardPositions[2] = 7;
-        powerAwardPositions[3] = 10;
         setupReligion(playerCount,initial_religion_points);
     }
 
 
     private void setupReligion(int playerCount, int[] initial_religion_points){
+        powerAwardPositions = new int[11];
+        for (int i = 0; i < playerPositions.length ; i++){
+            powerAwardPositions[i] = 0;
+        }
+        powerAwardPositions[3] = 1;
+        powerAwardPositions[5] = 2;
+        powerAwardPositions[7] = 2;
+        powerAwardPositions[10] = 3;
         for(int i = 0; i< playerCount; i++){
             updateReligion(initial_religion_points[i], i, false);
         }
@@ -39,11 +42,15 @@ public class Religion {
      * @return powerAward the amount of power player gained
      *
      */
-    private  int updateReligion(int count, int player_id, boolean key){
+    private int updateReligion(int count, int player_id, boolean key){
         int powerAward = 0;
         int currentPos = playerPositions[player_id];
         int endPos = currentPos + count;
         int awardSearchLength = powerAwardPositions.length;
+        if(currentPos >= 10){
+            System.out.println("Cannot advance more on this religion");
+            return -1; // these -1's can represent error messages or throw exceptions
+        }
         if (endPos >= 10){
             if (!key){
                System.out.println("Since there is no key end pos is stuck on 9"); // Can be replaced with an GUI message
@@ -52,30 +59,33 @@ public class Religion {
             }
         }
         for (int i = 0; i< awardSearchLength; i++ ){
-            if (currentPos < powerAwardPositions[i] && endPos >= powerAwardPositions[i]){
+            if (currentPos < i && endPos >= i){
                 powerAward += powerAwardPositions[i];
             }
         }
         playerPositions[player_id] = endPos;
         return powerAward;
     }
-    private int placePriest(int player_id,boolean key){
-        if(orderOfCult_3 != -1) {
-            orderOfCult_3 = player_id;
-            return this.updateReligion(3, player_id, key);
-        }else if (orderOfCult_2_1 != -1){
-            orderOfCult_2_1 = player_id;
-            return this.updateReligion(2, player_id, key);
-        }else if (orderOfCult_2_2 != -1){
-            orderOfCult_2_2 = player_id;
-            return this.updateReligion(2, player_id, key);
-        }else if (orderOfCult_2_3 != -1){
-            orderOfCult_2_1 = player_id;
-            return this.updateReligion(2, player_id, key);
-        }else
-            return -1; // Error value which indicates there is no empty place
+    public int placePriest(int player_id,boolean key) {
+        return updateReligion(1, player_id, key);
     }
-    private  int addOrderOfReligion(int player_id, boolean key){
-        return updateReligion(1,player_id, key);
+
+    public  int addOrderOfReligion(int player_id, boolean key){
+            if(orderOfCult_3 == -1) {
+                orderOfCult_3 = player_id;
+                return this.updateReligion(3, player_id, key);
+            }else if (orderOfCult_2_1 == -1){
+                orderOfCult_2_1 = player_id;
+                return this.updateReligion(2, player_id, key);
+            }else if (orderOfCult_2_2 == -1){
+                orderOfCult_2_2 = player_id;
+                return this.updateReligion(2, player_id, key);
+            }else if (orderOfCult_2_3 == -1){
+                orderOfCult_2_3 = player_id;
+                return this.updateReligion(2, player_id, key);
+            }else
+                System.out.println("ORDER IS FULL");
+                return -1; // Error value which indicates there is no empty place
+                             //// these -1's can represent error messages or throw exceptions
     }
 }
