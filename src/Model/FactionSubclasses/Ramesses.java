a7b911656f29dd07d7d3482f8c22e99ca4090064
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2020-03-16 21:35:46 +0300

    Update Ramesses.java

--- src/Model/FactionSubclasses/Ramesses.java
+++ src/Model/FactionSubclasses/Ramesses.java
@@ -2,5 +2,23 @@ package Model.FactionSubclasses;
 
 import Model.Faction;
 
-public class Ramesses extends Faction {
+public class Ramesses extends Faction { // FAKİR
+  
+  public Ramesses()
+  {
+        INITIAL_WORKER = 3;
+        INITIAL_GOLD = 15;
+        INITIAL_PRIEST = 0;
+        INITIAL_POWER = 0;
+        INITIAL_BUDISM = 1; // fire
+        INITIAL_ISLAM = 0;  //water
+        INITIAL_JUDAISM = 0;  //earth     
+        INITIAL_CHRISTIANITY = 1; //air
+        INITIAL_VICTORY_POINT = 33;
+        INITIAL_DWELLING_NUMBER = 2;
+        DWELLING_GOLD_COST = 2;
+        DWELLING_WORKER_COST = 1;
+        DWELLING_WORKER_INCOME = 1;
+  }
+    
 }
