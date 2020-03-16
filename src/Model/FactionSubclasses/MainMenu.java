4413ca985f22b83c8eeef3987feb8d9d35be193b
dalkiran70 <yumurcak232@gmail.com>
2020-03-16 18:22:43 +0300

    MainMenu class was created. The methods of class is empty for now. Methods are evaluated

---
+++ src/Model/FactionSubclasses/MainMenu.java
@@ -0,0 +1,36 @@
+package Model.FactionSubclasses;
+
+import Controller.GameHandler;
+import Model.Faction;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainMenu {
+    GameHandler gameHandler;
+    int numberOfPlayer;
+
+    public void startNewGame(ArrayList<Faction> factionList){
+
+    }
+    public void setSettings(int resolutionId, int volume){
+
+    }
+    public void showCredits(){
+
+    }
+    public void exit(){
+
+    }
+    public void loadGame(int id, File file){
+
+    }
+    public void saveGame(GameHandler gameHandler){
+
+    }
+    public void chooseFaction(int playerCount){
+
+    }
+
+}
+
