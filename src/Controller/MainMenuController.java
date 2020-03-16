a7d5778bb0e0fbcadb8ffd097eeb292ba4d00158
Can Aybalık <can.moonfish@gmail.com>
2020-03-16 21:34:16 +0300

    MainMenuView.fxml and MainMenuController.java are added

---
+++ src/Controller/MainMenuController.java
@@ -0,0 +1,148 @@
+package Controller;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.application.Application;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.application.Platform;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import static java.util.Objects.requireNonNull;
+
+public class MainMenuController   implements Initializable {
+
+    @FXML
+    Button createGameButton, loadGameButton, settingsButton, helpButton, creditsButton, exitButton, helpLink;
+
+
+
+
+    @FXML
+    public void createGameButtonClicked( MouseEvent event) {
+
+
+    }
+
+    @FXML
+    public void loadGameButtonClicked( MouseEvent event) {
+
+
+    }
+
+    @FXML
+    public void settingsButtonClicked( MouseEvent event) throws IOException {
+
+        Stage stage;
+
+
+        stage = (Stage) creditsButton.getScene().getWindow();
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("/sample/settingsMenu.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    @FXML
+    public void helpButtonClicked( MouseEvent event) throws IOException {
+
+        Stage stage;
+
+
+        stage = (Stage) creditsButton.getScene().getWindow();
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("/sample/helpMenu.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+    public static void openWebpage(String urlString) {
+        try {
+            Desktop.getDesktop().browse(new URL(urlString).toURI());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void helpLinkButtonClicked( MouseEvent event) {
+
+        openWebpage("http://www.feuerland-spiele.de/dateien/Terra_Mystica_EN_1.0_.pdf");
+
+    }
+
+
+    @FXML
+    public void creditsButtonClicked( MouseEvent event) throws Exception {
+
+        Stage stage;
+
+
+        stage = (Stage) creditsButton.getScene().getWindow();
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("/sample/creditsMenu.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+
+    @FXML
+    public void exitButtonClicked( MouseEvent event) {
+
+        Platform.exit();
+        System.exit(0);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    /*
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+        creditsButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("creditsMenu.fxml"));
+                    stage.getScene().setRoot(root);
+                    System.out.print("girdi");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+     */
+}
