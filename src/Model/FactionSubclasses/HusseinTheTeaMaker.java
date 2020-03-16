729e24fadeb3e8e699cb9ad39c8cbc4de9a84719
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2020-03-16 21:26:59 +0300

    Update HusseinTheTeaMaker.java

--- src/Model/FactionSubclasses/HusseinTheTeaMaker.java
+++ src/Model/FactionSubclasses/HusseinTheTeaMaker.java
@@ -2,5 +2,23 @@ package Model.FactionSubclasses;
 
 import Model.Faction;
 
-public class HusseinTheTeaMaker extends Faction {
+public class HusseinTheTeaMaker extends Faction { //halfling
+  
+  public HusseinTheTeaMaker()
+  {       
+        INITIAL_WORKER = 3;
+        INITIAL_GOLD = 15;
+        INITIAL_PRIEST = 0;
+        INITIAL_POWER = 0;
+        INITIAL_BUDISM = 0; // fire
+        INITIAL_ISLAM = 0;  //water
+        INITIAL_JUDAISM = 1;  //earth     
+        INITIAL_CHRISTIANITY = 1; //air
+        INITIAL_VICTORY_POINT = 20;
+        INITIAL_DWELLING_NUMBER = 2;
+        DWELLING_GOLD_COST = 2;
+        DWELLING_WORKER_COST = 1;
+        DWELLING_WORKER_INCOME = 1;
+  }
 }
+ 
