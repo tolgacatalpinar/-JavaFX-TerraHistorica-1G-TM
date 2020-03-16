c3c6361820936a95b03b51d2422d9d1d55d88e89
dalkiran70 <yumurcak232@gmail.com>
2020-03-16 18:39:51 +0300

    TownTile class was created and its attributes were added.

---
+++ src/Model/TownTile.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class TownTile {
+    int powerBonus;
+    int priestBonus;
+    int keyCount;
+    int victoryAdd;
+    int religionPoint;
+    boolean isShippingBonus;
+    int workerBonus;
+    int goldBonus;
+}
