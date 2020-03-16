1e8865fe16703a20a38b0433102113f998ad89ff
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2020-03-16 21:40:07 +0300

    Update VladTheImpaler.java

--- src/Model/FactionSubclasses/VladTheImpaler.java
+++ src/Model/FactionSubclasses/VladTheImpaler.java
@@ -2,13 +2,16 @@ package Model.FactionSubclasses;
 
 import Model.Faction;
 
-public class VladTheImpaler extends Faction {
+public class VladTheImpaler extends Faction { // chaos magician
     public VladTheImpaler() {
         INITIAL_WORKER = 4;
         INITIAL_GOLD = 15;
         INITIAL_PRIEST = 0;
         INITIAL_POWER = 0;
-        INITIAL_BUDISM = 2;
+        INITIAL_BUDISM = 2; //fire
+        INITIAL_ISLAM = 0;  //water
+        INITIAL_JUDAISM = 0;  //earth     
+        INITIAL_CHRISTIANITY = 0; //air
         INITIAL_VICTORY_POINT = 19;
         INITIAL_DWELLING_NUMBER = 1;
         DWELLING_GOLD_COST = 2;
