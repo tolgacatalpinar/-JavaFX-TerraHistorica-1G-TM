144d309252582ad7b086593c9f09b093e8144d9a
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2020-03-16 21:11:29 +0300

    Update DariusTheGreat.java

--- src/Model/FactionSubclasses/DariusTheGreat.java
+++ src/Model/FactionSubclasses/DariusTheGreat.java
@@ -2,5 +2,22 @@ package Model.FactionSubclasses;
 
 import Model.Faction;
 
-public class DariusTheGreat extends Faction {
+public class DariusTheGreat extends Faction { //nomad
+         
+        public DariusTheGreat ()
+       {
+        INITIAL_WORKER = 2;
+        INITIAL_GOLD = 15;
+        INITIAL_PRIEST = 0;
+        INITIAL_POWER = 0;
+        INITIAL_BUDISM = 1; // fire
+        INITIAL_ISLAM = 0;  //water
+        INITIAL_JUDAISM = 1;  //earth     
+        INITIAL_CHRISTIANITY = 0; //air
+        INITIAL_VICTORY_POINT = 19;
+        INITIAL_DWELLING_NUMBER = 3;
+        DWELLING_GOLD_COST = 2;
+        DWELLING_WORKER_COST = 1;
+        DWELLING_WORKER_INCOME = 1;
+       }
 }
