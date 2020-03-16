f0c4c8a2532cda8b4266df31365de341ab6d8ff8
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2020-03-16 21:17:57 +0300

    Update ErikTheRed.java

--- src/Model/FactionSubclasses/ErikTheRed.java
+++ src/Model/FactionSubclasses/ErikTheRed.java
@@ -3,4 +3,21 @@ package Model.FactionSubclasses;
 import Model.Faction;
 
 public class ErikTheRed extends Faction {
+  
+  public ErikTheRed()
+  {
+        INITIAL_WORKER = 8;
+        INITIAL_GOLD = 20;
+        INITIAL_PRIEST = 0;
+        INITIAL_POWER = 0;
+        INITIAL_BUDISM = 1; // fire
+        INITIAL_ISLAM = 1;  //water
+        INITIAL_JUDAISM = 1;  //earth     
+        INITIAL_CHRISTIANITY = 1; //air
+        INITIAL_VICTORY_POINT = 22;
+        INITIAL_DWELLING_NUMBER = 2;
+        DWELLING_GOLD_COST = 3;
+        DWELLING_WORKER_COST = 2;
+        DWELLING_WORKER_INCOME = 2;
+  }
 }
