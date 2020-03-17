8279824e5cd36251a48b12b3b3dbece8b10f6730
Can Aybalık <can.moonfish@gmail.com>
2020-03-17 19:42:32 +0300

    MainMenu resolution is changed

--- src/View/Main.java
+++ src/View/Main.java
@@ -15,8 +15,8 @@ public class Main extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("MainMenuView.fxml"));
         primaryStage.setTitle("Terra Historica");
-        primaryStage.setScene(new Scene(root, 1920, 1080));
-        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        //primaryStage.setMaximized(true);
         primaryStage.show();
 
     }
