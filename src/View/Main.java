caf16de22b87d83592c888c9d7369c91ba0a5b37
Can Aybalık <can.moonfish@gmail.com>
2020-03-18 10:51:33 +0300

    setResizable() method is added to the Main.java

--- src/View/Main.java
+++ src/View/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("MainMenuView.fxml"));
         primaryStage.setTitle("Terra Historica");
         primaryStage.setScene(new Scene(root, 1280, 720));
-        //primaryStage.setMaximized(true);
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
