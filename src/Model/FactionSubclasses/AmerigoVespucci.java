54fe429a6173cffcb24752b5d59651782a410070
Sena Korkut <sena123korkut123@gmail.com>
2020-03-22 01:54:25 +0300

    Few methods and variables implemented

--- src/Model/FactionSubclasses/AmerigoVespucci.java
+++ src/Model/FactionSubclasses/AmerigoVespucci.java
@@ -8,8 +8,8 @@ public class AmerigoVespucci extends Faction { // mermaids
   {
         INITIAL_ISLAM = 2;  //water
         INITIAL_VICTORY_POINT = 19;
-        INITIAL_LEVEL_ONE_POWER = 3;
-        INITIAL_LEVEL_ONE_POWER = 9;
+        INITIAL_BOWL_ONE_POWER = 3;
+        INITIAL_BOWL_TWO_POWER = 9;
         INITIAL_DWELLING_NUMBER = 2;
         DWELLING_GOLD_COST = 2;
         DWELLING_WORKER_COST = 1;
