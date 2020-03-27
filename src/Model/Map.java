5d124ddffb13ad148c74cb7cd3bb2021a3a7b3db
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2020-03-27 10:44:16 +0300

    Update Map.java

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -117,26 +117,182 @@ public class Map{
       return false;
    }
 
-   public void upgradeBuilding(Space space) {
-
-   }
-
-   public boolean canBuildBridge( Space space1, Space space2 ) {
-      return false;
-   }
-
    public boolean isTown(Space newBuilt) {
       return false;
    }
 
-   public void transformTerrain(Space space) {
-
-   }
-
-   public ArrayList<Integer> adjacentPlayer(Space space) {
-     return null;
-   }
 
 
+//    public int[] buildDwelling(Space space1, String color) {
+//        int[] location = null; /// burada belki hata olabilir
+//        if ( canBuild(space1, color)){ 
+//            location = new int[2];
+//            int row = getRow(space1);
+//            int column = getColumn(space1);
+//            location[0] = row;
+//            location[1] = column;
+//            space1.setIsOccupied(true);
+//        }
+//        return location;
+//    }
+
+//    public boolean isDirectAdjacent(Space space1, Space space2) {
+//       // We did not control if one of the spaces is river, because even one of them is river this info can be useful.
+//       int row1 = getRow(space1);
+//       int row2 = getRow(space2);
+//       int col1 = getColumn(space1);
+//       int col2 = getColumn(space2);
+//       if (row1 % 2 == 0){
+//           if ( ((row2 == row1 + 1) || (row2 == row1 - 1)) && ((col2 ==  col1 - 1) || (col2 == col1))){
+//            return true;
+//           }
+//       }
+
+//       if (row1 % 2 == 1){
+//            if ( ((row2 == row1 + 1) || (row2 == row1 - 1)) && ((col2 ==  col1 + 1) || (col2 == col1))){
+//            return true;
+//            }
+//       }
+
+//       if((row1 == row2) && ((col1 == col2 + 1) || (col1 == col2 - 1))){
+//           return true;
+//       }
+
+//       return false;
+//    }
+
+
+   
+//    public Space[] adjacencyList(Space space1)
+//    {
+//        int[] adjacents = new int[6];
+//        int row, column = -1;
+//        int j = 0;
+       
+//        for(int i = 0; i < 117; i++) {
+//           row = i / 13;
+//           column = i % 13;
+//           if (isDirectAdjacent(space1,spaces[row][column])){
+//              adjacents[j] = spaces[row][column];
+//              j++;
+//           }
+//        }
+//        return adjacents;
+//    }
+
+
+//    public boolean canBuildBridge( Space space1, Space space2 ) {
+//       int row1 = getRow(space1);
+//       int row2 = getRow(space2);
+//       int col1 = getColumn(space1);
+//       int col2 = getColumn(space2);
+      
+//       // bridge case 1
+//       if (math.abs(row1 - row2) == 2 && col1 == col2){
+//          if (row1 % 2 == 0){
+//             if (spaces[(row1+row2)/2][col1] instanceof River && spaces[(row1+row2)/2][col1 - 1] instanceof River)
+//                return true;
+//          }
+//          else{
+//             if (spaces[(row1+row2)/2][col1] instanceof River && spaces[(row1+row2)/2][col1 + 1] instanceof River)
+//                return true;
+//          }
+//       }
+      
+//       // bridge case 2
+//      if (math.abs(row1 - row2) == 1){
+//       if((col2 == col1 + 1) && (spaces[row1][col1 + 1] instanceof River && spaces[row + 1][col1] instanceof River))
+//             return true;
+//       else if ((col2 == col1 - 2) && (spaces[row1][col1 - 1] instanceof River && spaces[row - 1][col1 - 1] instanceof River))
+//             return true;
+//       else if ((col2 == col1 - 2) && (spaces[row1][col1 - 1] instanceof River && spaces[row + 1][col1 - 1] instanceof River))
+//             return true;
+//       else if ((col2 == col1 + 1) && (spaces[row1][col1 + 1] instanceof River && spaces[row - 1][col1] instanceof River))
+//             return true;
+//       /////
+//       else if((col2 == col1 + 2) && (spaces[row1][col1 + 1] instanceof River && spaces[row + 1][col1 + 1] instanceof River))
+//             return true;
+//       else if ((col2 == col1 - 1) && (spaces[row1][col1 - 1] instanceof River && spaces[row - 1][col1] instanceof River))
+//             return true;
+//       else if ((col2 == col1 - 1) && (spaces[row1][col1 - 1] instanceof River && spaces[row + 1][col1] instanceof River))
+//             return true;
+//       else if ((col2 == col1 + 2) && (spaces[row1][col1 + 1] instanceof River && spaces[row - 1][col1 + 1] instanceof River))
+//             return true;
+//      }
+//      return false;
+//    }
+   
+//    public void buildBridge(Space space1, Space space2){
+//       if(canBuildBridge(space1, space2)){
+//          space1.setBridge(true);
+//          space1.buildBridge(space2);
+//          space2.buildBridge(space1);
+//          space2.setBridge(true);
+//          System.out.println("Yeni köprümüz hayırlı uğurlu olsun");
+//       }
+//    }
+     
+//    public boolean isTown(Space newBuilt) {
+//       return false;
+//    }
+
+//    public void transformTerrain(Space original, Space newSpace) {
+//       int row = getRow(original);
+//       int col = getColumn(original);
+//       Spaces[row][col] = newSpace;
+//       original = null;
+//    }
+   
+//    public boolean UpgradeStructure(Space space1, String playerColor, int upgradeType){
+//       if(space1.getColor == playerColor && space1.getIsOccupied()){
+//        if( choice == 1 && space1.getStructure() instanceof Dwelling)
+//           return true;
+//        if( choice == 2 && space1.getStructure() instanceof TradingHouse)  
+//           return true;
+//        if( choice == 3 && space1.getStructure() instanceof TradingHouse)  
+//           return true;
+//        if( choice == 4 && space1.getStructure() instanceof Temple)  
+//           return true;
+//       }
+      
+//       else
+//          return false;  
+//    }
+   
+                               
+//    public ArrayList<Integer> adjacentPlayer(Space space, String playerColor) {
+//       int[] adjacents = adjacencyList(space);
+//       ArrayList<Space> list=new ArrayList<Space>();
+//       for (int i = 0; i < adjacents.length; i++){
+//          if (adjacents[i].getColor() != playerColor && adjacents[i].isOccupied()){
+//             list.add(adjacents[i]);
+//          }
+//       }
+//       return list;
+//    }
+   
+//    public boolean canBuild(Space space1, String playerColor){
+//       if(space1.getColor() == playerColor && !space1.isOccupied()){
+//          int[] adjacents = adjacencyList(space1);
+//          for (int i = 0; i < adjacents.length; i++){
+//             if (adjacents[i].getColor() == playerColor && adjacents[i].isOccupied()){
+//                return true;
+//             }
+//          }
+         
+//          Space[] bridgeList = space1.getBridgeList();
+//          if( bridgeList[0].getColor() == playerColor && bridgeList[0].isOccupied())   
+//             return true;
+//       }
+//       return false;
+//    }
+   
+      
+//    public boolean canBuildTurnOne(Space space1, String playerColor){
+//       if(space1.getColor() == playerColor && !space1.isOccupied()){
+//          return true;
+//       }
+//       return false;
+//    }
 
 }
