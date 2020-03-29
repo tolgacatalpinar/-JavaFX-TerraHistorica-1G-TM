1882d005dd699e874b1d6871334e9400a54f9d1f
Can Aybalık <can.moonfish@gmail.com>
2020-03-29 14:52:41 +0300

    Terrain Images are changed

--- src/Controller/MapController.java
+++ src/Controller/MapController.java
@@ -15,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -31,7 +32,7 @@ public class MapController extends Application implements Initializable {
    @FXML
    Pane mapPane;
    @FXML
-   BorderPane mapBorderPane;
+   VBox mapVBox;
    Button[][] terrains;
    Map map;
 
