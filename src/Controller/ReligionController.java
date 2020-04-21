907ac18858e8c79d0b5daed4ee2125a6e49af364
Caglar Cankaya <caglarcankaya@yandex.com>
2020-04-22 02:26:52 +0300

    Religion GUI evolved!

--- src/Controller/ReligionController.java
+++ src/Controller/ReligionController.java
@@ -24,6 +24,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.Glow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -44,6 +46,8 @@ public class ReligionController extends Application{
 
     @FXML
     private GridPane myOrder;
+    @FXML
+    private Pane main_pane;
 
     private int[] array = {2, 3, 5};
     private int currentPlayer = 0;
@@ -59,44 +63,10 @@ public class ReligionController extends Application{
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/View/ReligionView.fxml"));
         gridPane = (GridPane) root.getChildrenUnmodifiable().get(0);
-
-        Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                Runnable updater = new Runnable() {
-
-                    @Override
-                    public void run() {
-                        update(gridPane,1,true);
-                    }
-                };
-
-                while (true) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex) {
-                    }
-
-                    // UI update is run on the Application thread
-                    Platform.runLater(updater);
-                }
-            }
-
-        });
-        // don't let thread prevent JVM shutdown
-        thread.setDaemon(true);
-        //thread.start();
-
         update(gridPane,1,false);
 
         //Find religion to add as size (y coordinate)%(1/4 of anchor pane's size) to replace choice box.
         choiceBox = (ChoiceBox<String>) root.getChildrenUnmodifiable().get(1);
-        //choiceBox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
-        //    @Override
-        //    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-        //    }
-        //});
         priestButton = (Button) root.getChildrenUnmodifiable().get(3);
         priestButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -123,8 +93,6 @@ public class ReligionController extends Application{
         primaryStage.setScene(new Scene(root, 1920, 1080));
         primaryStage.setMaximized(true);
         primaryStage.show();
-
-
     }
     /**
      * According to religion track's data paint the locations occupied
@@ -137,95 +105,89 @@ public class ReligionController extends Application{
         for(int i = 0; i < 4; i++){
             for (int j = 0; j<12; j++){
                 GridPane tempGrid = new GridPane();
-                //tempGrid.setPrefSize(gridPane.getWidth()/12,gridPane.getHeight()/4);
-                tempGrid.setStyle("-fx-background-color: gray, -fx-control-inner-background; -fx-background-insets: 0, 2; -fx-padding: 2;");
+                Label text = new Label();
+                if(j >= 1){
+                    if(i == 1){
+                        BackgroundImage myBI= new BackgroundImage(new Image("Images/ReligionImages/chris_track.png",tempGrid.getWidth(),tempGrid.getHeight(),false,true),
+                                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                                BackgroundSize.DEFAULT);
+                        //then you set to your node
+                        tempGrid.setBackground(new Background(myBI));
+                    }else if(i == 0){
+                        BackgroundImage myBI= new BackgroundImage(new Image("Images/ReligionImages/islam_track.png",tempGrid.getWidth(),tempGrid.getHeight(),false,true),
+                                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                                BackgroundSize.DEFAULT);
+                        //then you set to your node
+                        tempGrid.setBackground(new Background(myBI));
+                    }else if( i == 3){
+                        BackgroundImage myBI= new BackgroundImage(new Image("Images/ReligionImages/hindu_track.png",tempGrid.getWidth(),tempGrid.getHeight(),false,true),
+                                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                                BackgroundSize.DEFAULT);
+                        //then you set to your node
+                        tempGrid.setBackground(new Background(myBI));
+                    }else{
+                        BackgroundImage myBI= new BackgroundImage(new Image("Images/ReligionImages/juda_track.png",tempGrid.getWidth(),tempGrid.getHeight(),false,true),
+                                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                                BackgroundSize.DEFAULT);
+                        //then you set to your node
+                        tempGrid.setBackground(new Background(myBI));
+                    }
+                }else {
+                    BackgroundImage myBI= new BackgroundImage(new Image("Images/ReligionImages/islam_door.png",100,180,false,true),
+                            BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                            BackgroundSize.DEFAULT);
+                    //then you set to your node
+                    tempGrid.setBackground(new Background(myBI));
+                }
                 for(int k = 0; k<4; k++){
                     Pane newPane = new Pane();
                     newPane.setPrefSize(tempGrid.getWidth()/4,tempGrid.getHeight()/4);
                     newPane.setPrefSize(50,90);
-                    //newPane.setStyle("-fx-background-color: blue");
-                    //newPane.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
-                    newPane.setBackground(Background.EMPTY);
                     tempGrid.add(newPane, k/2, k%2);
                 }
-
                 gridPane.add(tempGrid, j,i);
-
+                if(j > 0){
+                    final Effect glow = new Glow(100.0);
+                    text.setEffect(glow);
+                    text.setText("        "+(j-1));
+                    text.setFont(new Font("Stencil", 20));
+                    text.setTextFill(Color.LIGHTGREY);
+                    if(j == 3 || j == 6 || j== 9 || j==11){
+                        ImageView powerImage = new ImageView("Images/ReligionImages/islam_symbol.png");
+                        powerImage.setFitHeight(100);
+                        powerImage.setFitWidth(10);
+                        gridPane.add(powerImage,j,i);
+                    }
+                    gridPane.add(text, j,i);
+                }
             }
         }
+
         Color[] colors = {Color.BLUE, Color.RED,Color.GREEN};
-        //Area without order colorizing
-      // for(int i = 0; i< religions.length; i++){
-      //     for (int j = 0; j < playerCount; j++){
-      //         int position = religions[i].getPlayerPositions()[j];
-      //         GridPane newPane = (GridPane)getNodeByRowColumnIndex(i,position+1,gridPane);
-      //         //newPane.setBackground(Background.EMPTY);
-      //         Pane playerPane = (Pane) getNodeByRowColumnIndex(1,1,newPane);
-      //         playerPane.setBackground(new Background(new BackgroundFill(colors[j], CornerRadii.EMPTY, Insets.EMPTY)));
-      //        System.out.println("Positions: " + (position+1) + " and " + i);
-      //     }
-      // }
        for(int i = 0; i< religions.length; i++){
            for (int j = 0; j < playerCount; j++){
                int position = religions[i].getPlayerPositions()[j];
                GridPane newPane = (GridPane)getNodeByRowColumnIndex(i,position+1,gridPane);
                Pane playerPane = (Pane) getNodeByRowColumnIndex(j/2,j%2,newPane);
-               //newPane.setBackground(Background.EMPTY);
                playerPane.setBackground(new Background(new BackgroundFill(colors[j], CornerRadii.EMPTY, Insets.EMPTY)));
                System.out.println("Positions: " + (position+1) + " and " + i);
            }
        }
-        for(int k = 0; k < 4; k++){
-            for (int i = 0; i < 4; i++) {
-                Pane orderPane = new Pane();
-                orderPane.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
-                if (religions[k].isOccupied(i)) {
-                    GridPane orderGridPane = (GridPane) gridPane.getChildren().get(k*12);
-                    //orderGridPane.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
-                    orderGridPane.add(orderPane, i % 2, i / 2);
-                }
-            }
-        }
-
-
-
-
-       //  System.out.println(gridPane.getRowCount());
-       //  System.out.println(gridPane.getColumnCount());
-//     int limit = 11;
-//     Pane pane = new Pane();
-//     int nextSpace = 0;
-//     // TO MOVE PRIEST
-//     if (!isOrder) {
-//        nextSpace = religions[religion_index].getPlayerPositions()[currentPlayer];
-//        System.out.println("Next space to move priest is: " + nextSpace);
-//
-//          if (nextSpace != limit) {
-//             //System.out.println("Religion count: " + religions[religion_index].);
-//             pane.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-//             switch (religion_index) {
-//                case 0:
-//                   pane.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
-//                   break;
-//                case 1:
-//                   pane.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-//                   break;
-//                case 2:
-//                   pane.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-//                   break;
-//                case 3:
-//                   pane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-//                   break;
-//             }
-//             gridPane.add(pane, nextSpace, religion_index);
-//          }
-//       } else {
-//          //pane.setBackground(new Background(new BackgroundFill(Color.INDIGO, CornerRadii.EMPTY, Insets.EMPTY)));
-//          //religions[religion_index].addOrderOfReligion(0, true);
-//          //nextSpace = religions[religion_index].getPlayerPositions()[0];
-//          //gridPane.add(pane, nextSpace, religion_index);
-//
-//       }
+       for(int k = 0; k < 4; k++){
+           for (int i = 0; i < 4; i++) {
+               Pane orderPane = new Pane();
+               orderPane.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
+               if (religions[k].isOccupied(i)) {
+                   GridPane orderGridPane = (GridPane) gridPane.getChildren().get(k*12);
+                   BackgroundImage myBI= new BackgroundImage(new Image("Images/ReligionImages/islam_door.png",100,180,false,true),
+                           BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                           BackgroundSize.DEFAULT);
+                   //then you set to your node
+                   orderGridPane.setBackground(new Background(myBI));
+                   orderGridPane.add(orderPane, i % 2, i / 2);
+               }
+           }
+       }
     }
     public Node getNodeByRowColumnIndex (final int row, final int column, GridPane gridPane) {
         Node result = null;
