72b7457227f70de82edd4704e284db1612031979
dalkiran70 <yumurcak232@gmail.com>
2020-04-22 15:43:58 +0300

    FavorTile view are redesigned

--- src/View/FavorTileView.java
+++ src/View/FavorTileView.java
@@ -31,50 +31,62 @@ public class FavorTileView extends HBox {
       addReligionToView();
       setStyle("-fx-border-color: red;\n" +
               "-fx-padding: 30;\n" +
-              "-fx-border-width: 3;\n" +
+              "-fx-border-width: 5;\n" +
               "-fx-border-style: solid;\n");
       if( this.favorTile.getPowerBonus() > 0)
-         this.getChildren().add( new Label("Power: " + this.favorTile.getPowerBonus()));
-      else if(this.favorTile.getWorkerBonus() > 0)
-         this.getChildren().add( new Label("Worker: " + this.favorTile.getWorkerBonus()));
-      else if(this.favorTile.getGoldBonus() > 0)
-         this.getChildren().add( new Label("Gold: " + this.favorTile.getGoldBonus()));
-      else if(this.favorTile.isSpecialCult())
+         this.getChildren().add( new Label("Power : " + this.favorTile.getPowerBonus()));
+      if(this.favorTile.getWorkerBonus() > 0)
+         this.getChildren().add( new Label("Worker : " + this.favorTile.getWorkerBonus()));
+      if(this.favorTile.getGoldBonus() > 0)
+         this.getChildren().add( new Label("Gold : " + this.favorTile.getGoldBonus()));
+      if(this.favorTile.isSpecialCult())
          this.getChildren().add( new Label("Special Cult"));
-      else if(this.favorTile.isTradingHouse())
-         this.getChildren().add(new Label("Trading - Vict: " + this.favorTile.getVictoryPoint()));
-      else if(this.favorTile.isDwellingBonus())
-         this.getChildren().add( new Label("Dwelling - Vict: " + this.favorTile.getVictoryPoint()));
-      else if(this.favorTile.isTownBonus())
+      if(this.favorTile.isTradingHouse())
+         this.getChildren().add(new Label("Trading - Vict : " + this.favorTile.getVictoryPoint()));
+      if(this.favorTile.isDwellingBonus())
+         this.getChildren().add( new Label("Dwelling - Vict : " + this.favorTile.getVictoryPoint()));
+      if(this.favorTile.isTownBonus())
          this.getChildren().add( new Label("Town bonus"));
+      if (this.favorTile.isPassingBonusForTradingHouse())
+         this.getChildren().add(new Label("Passing Trade House Bonus" ));
 
 
-
+      addPlayerNumberView();
       setMinHeight(130);
       setMinWidth(220);
       setMargin(this, new Insets(0, 30, 30, 0));
       setSpacing(30);
       setBackground( new Background( new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
    }
-
+   public void addPlayerNumberView(){
+      VBox playerNumber = null;
+      String count = "";
+      for (int i = 0; i < this.favorTile.getNumberOfPlayer(); i++){
+         count = count + "#";
+      }
+      playerNumber = new VBox();
+      Label playerNum = new Label(count);
+      playerNumber.getChildren().add(playerNum);
+      this.getChildren().add(playerNumber);
+   }
    public void addReligionToView()
    {
       VBox religionBox = null;
       if(favorTile.getIslamBonus() > 0)
       {
-         religionBox = religionBlock("Islam", favorTile.getIslamBonus());
+         religionBox = religionBlock("Islam Bonus : ", favorTile.getIslamBonus());
       }
       else if(favorTile.getChristianityBonus() > 0)
       {
-         religionBox = religionBlock("Christianity", favorTile.getChristianityBonus());
+         religionBox = religionBlock("Christianity Bonus : ", favorTile.getChristianityBonus());
       }
       else if(favorTile.getHinduismBonus() > 0)
       {
-         religionBox = religionBlock("Hinduism", favorTile.getHinduismBonus());
+         religionBox = religionBlock("Hinduism Bonus : ", favorTile.getHinduismBonus());
       }
       else if( favorTile.getJewBonus() > 0)
       {
-         religionBox = religionBlock("Jew", favorTile.getJewBonus());
+         religionBox = religionBlock("Jew Bonus : ", favorTile.getJewBonus());
       }
       if(religionBox != null)
          getChildren().add(religionBox);
@@ -82,11 +94,8 @@ public class FavorTileView extends HBox {
    public VBox religionBlock(String religionType, int count)
    {
       VBox religionBox = new VBox();
-      for( int i = 0; i < count; i ++)
-      {
-         Label religion = new Label(religionType);
-         religionBox.getChildren().add(religion);
-      }
+      Label religion = new Label(religionType + count );
+      religionBox.getChildren().add(religion);
       return religionBox;
    }
 }
