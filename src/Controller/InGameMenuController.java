3217570bc2c5d7d7150ee5df2d68d834448fb7ee
Caglar Cankaya <caglarcankaya@yandex.com>
2020-05-14 01:39:33 +0300

    Model code review changes according to design

---
+++ src/Controller/InGameMenuController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+
+public class InGameMenuController {
+    Slider volumeSlider;
+    Button setButton;
+    ChoiceBox resolutions;
+    //TODO
+    public int setVolume(int volume){
+        return volume;
+    }
+    //TODO
+    public void setResolution(int resolutionX, int resolutionY) {
+
+    }
+}
