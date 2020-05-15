18f1a05d8a5485de0a2b00eccdfce193866afd24
can-aybalik <61124496+can-aybalik@users.noreply.github.com>
2020-05-16 00:11:39 +0300

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module TerraHistoricaFX {
+
+  requires  javafx.fxml;
+  requires javafx.controls;
+
+
+  exports Controller;
+  opens Controller to javafx.fxml;
+//   opens Controller to javafx.graphics;
+  opens View to javafx.graphics;
+}
