9ff530e4694086526427d49db3d420d9c33c4a36
Can Aybalık <can.moonfish@gmail.com>
2020-05-17 18:06:36 +0300

    PlayerView is updated

--- src/View/PlayerView.java
+++ src/View/PlayerView.java
@@ -36,7 +36,7 @@ public class PlayerView extends BorderPane {
       //BackgroundImage bgImg = new BackgroundImage( imProfile, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(100,100,true,true,false, true));
 
       //Background bg = new Background(bgImg);
-      getStyleClass().add("test");
+
 
       //this.setStyle("-fx-background-color: RED");
 
@@ -57,45 +57,59 @@ public class PlayerView extends BorderPane {
       if (faction instanceof AliesterCrowley) {
          Image image = new Image("file:src/Images/FactionImages/Image_AleisterCrowley.jpeg");
          addImage(image);
+         getStyleClass().add("swamp");
       } else if (faction instanceof AmerigoVespucci) {
          Image image = new Image("file:src/Images/FactionImages/Image_AmerigoVespucci.jpeg");
          addImage(image);
+         getStyleClass().add("lakes");
       } else if (faction instanceof Buddha) {
          Image image = new Image("file:src/Images/FactionImages/Image_Buddha.jpeg");
          addImage(image);
+         getStyleClass().add("plains");
       } else if (faction instanceof DariusTheGreat) {
          Image image = new Image("file:src/Images/FactionImages/Image_DariusTheGreat.jpeg");
          addImage(image);
+         getStyleClass().add("desert");
       } else if (faction instanceof ErikTheRed) {
          Image image = new Image("file:src/Images/FactionImages/Image_ErikTheRed.jpeg");
          addImage(image);
+         getStyleClass().add("lakes");
       } else if (faction instanceof Gilgamesh) {
          Image image = new Image("file:src/Images/FactionImages/Image_Gilgamesh.jpeg");
          addImage(image);
+         getStyleClass().add("wasteland");
       } else if (faction instanceof HelenOfTroy) {
          Image image = new Image("file:src/Images/FactionImages/Image_HelenOfTroy.jpeg");
          addImage(image);
+         getStyleClass().add("forest");
       } else if (faction instanceof HusseinTheTeaMaker) {
          Image image = new Image("file:src/Images/FactionImages/Image_HusseinTheTeaMaker.jpeg");
          addImage(image);
+         getStyleClass().add("plains");
       } else if (faction instanceof LeonardoDaVinci) {
          Image image = new Image("file:src/Images/FactionImages/Image_LeonardoDaVinci.jpeg");
          addImage(image);
+         getStyleClass().add("mountains");
       } else if (faction instanceof MarieCurie) {
          Image image = new Image("file:src/Images/FactionImages/Image_MarieCurie.jpeg");
          addImage(image);
+         getStyleClass().add("swamp");
       } else if (faction instanceof MorganLeFay) {
          Image image = new Image("file:src/Images/FactionImages/Image_MorganLeFay.jpeg");
          addImage(image);
+         getStyleClass().add("swamp");
       } else if (faction instanceof Ramesses) {
          Image image = new Image("file:src/Images/FactionImages/Image_Ramesses.jpeg");
          addImage(image);
+         getStyleClass().add("desert");
       } else if (faction instanceof StPatrick) {
          Image image = new Image("file:src/Images/FactionImages/Image_StPatrick.jpeg");
          addImage(image);
+         getStyleClass().add("mountains");
       } else if (faction instanceof VladTheImpaler) {
          Image image = new Image("file:src/Images/FactionImages/Image_VladTheImpaler.jpeg");
          addImage(image);
+         getStyleClass().add("wasteland");
       }
       //addIncomes();
       //addResources();
@@ -104,6 +118,7 @@ public class PlayerView extends BorderPane {
    }
 
    public void addImage(Image image) {
+
       imageView.setImage(image);
       imageView.setFitWidth(80);
       imageView.setFitHeight(120);
@@ -111,6 +126,7 @@ public class PlayerView extends BorderPane {
       setCenter(imageView);
       //this.insets
       //setMargin(this, new Insets(60));
+
    }
    public void addAllResources()
    {
@@ -140,7 +156,10 @@ public class PlayerView extends BorderPane {
       resourceImage.setFitWidth(IMAGE_SIZE);
       resourceImage.setFitHeight(IMAGE_SIZE);
       ResourceLabel label = new ResourceLabel("\t" + number1);
+
       label.setTextFill(Color.WHITE);
+      label.getStyleClass().add("textShadow");
+
       resourceBox.getChildren().addAll(resourceImage, label);
       return resourceBox;
    }
@@ -152,8 +171,14 @@ public class PlayerView extends BorderPane {
       resourceImage.setFitHeight(IMAGE_SIZE);
       ResourceLabel current = new ResourceLabel("\t" + number1);
       ResourceLabel in = new ResourceLabel("\t+" + number2);
+
+
       current.setTextFill(Color.WHITE);
+      current.getStyleClass().add("textShadow");
+
       in.setTextFill(Color.GREEN);
+      in.getStyleClass().add("textShadow");
+
       resourceBox.getChildren().addAll(resourceImage, current, in);
       return resourceBox;
    }
@@ -168,9 +193,17 @@ public class PlayerView extends BorderPane {
       ResourceLabel label3 = new ResourceLabel("\t" + number3);
       ResourceLabel label4 = new ResourceLabel("\t+" + number4);
       label1.setTextFill(Color.WHITE);
+      label1.getStyleClass().add("textShadow");
+
       label2.setTextFill(Color.WHITE);
+      label2.getStyleClass().add("textShadow");
+
       label3.setTextFill(Color.WHITE);
+      label3.getStyleClass().add("textShadow");
+
       label4.setTextFill(Color.GREEN);
+      label4.getStyleClass().add("textShadow");
+
       resourceBox.getChildren().addAll(resourceImage, label1, label2, label3, label4);
       return resourceBox;
    }
