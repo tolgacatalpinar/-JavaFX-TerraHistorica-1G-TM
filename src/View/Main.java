343d72cc41531b20435bf6f3969405de764a5d24
Tolga Çatalpınar <34005953+tolgacatalpinar@users.noreply.github.com>
2020-05-17 22:08:31 +0300

    main menu updated

--- src/View/Main.java
+++ src/View/Main.java
@@ -42,7 +42,8 @@ public class Main extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("MenuViews/MainMenuView.fxml"));
         primaryStage.setTitle("Terra Historica");
-        primaryStage.setScene(new Scene(root, 1920, 1080));
+        primaryStage.setScene(new Scene(root, 800 , 600));
+        primaryStage.setMaximized(true);
         primaryStage.setResizable(false);
 //        primaryStage.setScene(new Scene(root, screenWidth, screenHeight));
 //        primaryStage.setResizable(true);
